3bcee6618e2aaa80481ff9626dc469f23c3ddc96
Svetlana Korbut <tebba@yandex.ru>
2023-06-14 22:57:58 +0200

    feat:add solutions to create.js module

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { appendFile } from 'node:fs/promises';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+
 const create = async () => {
-    // Write your code here 
+  const path = join(dirname(fileURLToPath(import.meta.url)), 'files', 'fresh.txt');
+  const context = 'I am fresh and young';
+  try {
+    await appendFile(path, context, { encoding: 'utf8', mode: 0o666, flag: 'ax' });
+  } catch ({ code }) {
+    if (code === 'EEXIST') {
+      console.error("FS operation failed");
+    }
+  }
 };
 
 await create();
