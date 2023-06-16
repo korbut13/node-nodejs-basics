752b4832d36af5f5495a93cfe5103b44175bed57
Svetlana Korbut <tebba@yandex.ru>
2023-06-16 14:02:04 +0200

    feat:add solution to rename.js and new solution to copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,33 +1,33 @@
-import { access, mkdir, readdir, copyFile, constants } from 'node:fs/promises';
+import { access, mkdir, readdir, copyFile } from 'node:fs';
 import { join, dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const copy = async () => {
   const srcPath = join(dirname(fileURLToPath(import.meta.url)), 'files');
-  const destPath = join(dirname(fileURLToPath(import.meta.url)), 'files_copy')
-  try {
-    await access(srcPath);
-    try {
-      if (await access(destPath) === undefined) {
-        throw new Error('FS operation failed');
-      }
-    } catch (err) {
-      if (err.code === 'ENOENT') {
-        await mkdir(destPath, { recursive: false });
-        const filesForCopy = await readdir(srcPath);
-        for (const file of filesForCopy) {
-          const sourceFile = join(srcPath, file);
-          const targetFile = join(destPath, file);
-          await copyFile(sourceFile, targetFile);
-        }
-      } else {
-        throw new Error('FS operation failed');
-      }
-    }
-  } catch (err) {
-    console.error('FS operation failed');
-  }
+  const destPath = join(dirname(fileURLToPath(import.meta.url)), 'files_copy');
 
+  access(destPath, (err) => {
+    if (err) {
+      mkdir(destPath, (err) => {
+        if (err) throw err;
+      });
+      readdir(srcPath, (err, files) => {
+        if (err) {
+          throw new Error('FS operation failed')
+        } else {
+          for (const file of files) {
+            const sourceFile = join(srcPath, file);
+            const targetFile = join(destPath, file);
+            copyFile(sourceFile, targetFile, (err) => {
+              if (err) throw err;
+            })
+          }
+        }
+      });
+    } else {
+      throw new Error('FS operation failed')
+    }
+  })
 };
 
 await copy();
