5515143b4cd9774dcef218c6ce1ddfbdca37ab6e
Svetlana Korbut <tebba@yandex.ru>
2023-06-18 19:58:50 +0200

    feat:add solutions to args.js and env.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+  const args = argv.slice(2);
+  let result = '';
+  args.forEach((arg, ind) => {
+    result = ind === 0 || ind % 2 === 0 ? `${result + arg.slice(2)} is ${args[ind + 1]}, ` : `${result + ''}`
+  })
+  console.log(result.trim().slice(0, -1));
 };
 
 parseArgs();
