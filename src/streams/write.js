fef2da70715317a9a98390f85240e9f5d98ed8b7
Svetlana Korbut <tebba@yandex.ru>
2023-06-19 22:39:03 +0200

    feat:add solution to stream transform.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -5,7 +5,8 @@ import { fileURLToPath } from 'node:url';
 
 const write = async () => {
   const destination = join(dirname(fileURLToPath(import.meta.url)), 'files', 'fileToWrite.txt');
-  const output = createWriteStream(destination);
+  const output = createWriteStream(destination, 'utf-8');
+  output.on('error', (error) => console.error(error));
   stdin.on('data', (chunk) => output.write(chunk));
 };
 
