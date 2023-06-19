eb10d94b89e73d6951c07830cc2f9c1a810de4bb
Svetlana Korbut <tebba@yandex.ru>
2023-06-19 21:25:03 +0200

    feat:add solution to stream read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import { createReadStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const read = async () => {
-    // Write your code here 
+  const src = join(dirname(fileURLToPath(import.meta.url)), 'files', 'fileToRead.txt');
+  const stream = createReadStream(src, 'utf-8');
+  stream.on('data', chunk => console.log(chunk));
+  stream.on('end', () => { });
+  stream.on('error', (error) => { console.error(error) })
 };
 
 await read();
