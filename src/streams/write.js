c0b560afeb4d775f7091c72d520e3fd7adf9a027
Svetlana Korbut <tebba@yandex.ru>
2023-06-19 21:46:05 +0200

    feat:add solution to stream write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import { createWriteStream } from 'node:fs';
+import { stdin } from 'node:process'
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const write = async () => {
-    // Write your code here 
+  const destination = join(dirname(fileURLToPath(import.meta.url)), 'files', 'fileToWrite.txt');
+  const output = createWriteStream(destination);
+  stdin.on('data', (chunk) => output.write(chunk));
 };
 
 await write();
